396678a171acff832d678dd65262950d1323897b
unknown <andypythonappdeveloper@gmail.com>
2021-04-25 19:03:40 +1000

    Refactor GDT to use bitfields

--- src/aero_kernel/src/arch/x86_64/gdt.rs
+++ src/aero_kernel/src/arch/x86_64/gdt.rs
@@ -4,21 +4,87 @@
 
 use core::mem;
 
-const GDT_ENTRIES: usize = 6;
+const GDT_ENTRIES: usize = 7;
 
-static mut GDT: [GdtEntry; GDT_ENTRIES] = [GdtEntry::NULL; GDT_ENTRIES];
+static mut GDT: [GdtEntry; GDT_ENTRIES] = [
+    // GDT null descriptor.
+    GdtEntry::NULL,
+    // GDT kernel code descriptor.
+    GdtEntry::new(
+        GdtAccessFlags::PRESENT
+            | GdtAccessFlags::RING_0
+            | GdtAccessFlags::SYSTEM
+            | GdtAccessFlags::EXECUTABLE
+            | GdtAccessFlags::PRIVILEGE,
+        GdtEntryFlags::LONG_MODE,
+    ),
+    // GDT kernel data descriptor.
+    GdtEntry::new(
+        GdtAccessFlags::PRESENT
+            | GdtAccessFlags::RING_0
+            | GdtAccessFlags::SYSTEM
+            | GdtAccessFlags::PRIVILEGE,
+        GdtEntryFlags::LONG_MODE,
+    ),
+    // GDT user data descriptor.
+    GdtEntry::new(
+        GdtAccessFlags::PRESENT
+            | GdtAccessFlags::RING_3
+            | GdtAccessFlags::SYSTEM
+            | GdtAccessFlags::PRIVILEGE,
+        GdtEntryFlags::LONG_MODE,
+    ),
+    // GDT user code descriptor.
+    GdtEntry::new(
+        GdtAccessFlags::PRESENT
+            | GdtAccessFlags::RING_3
+            | GdtAccessFlags::SYSTEM
+            | GdtAccessFlags::EXECUTABLE
+            | GdtAccessFlags::PRIVILEGE,
+        GdtEntryFlags::LONG_MODE,
+    ),
+    // GDT TSS descriptor.
+    GdtEntry::new(
+        GdtAccessFlags::PRESENT | GdtAccessFlags::RING_3 | GdtAccessFlags::TSS_AVAIL,
+        GdtEntryFlags::NULL,
+    ),
+    // GDT null descriptor as the TSS should be 16 bytes long
+    // and twice the normal size.
+    GdtEntry::NULL,
+];
 
 bitflags::bitflags! {
     /// Specifies which element to load into a segment from
     /// descriptor tables (i.e., is a index to LDT or GDT table
     /// with some additional flags).
-    pub struct SegmentSelector: u16 {
+    struct SegmentSelector: u16 {
         const RPL_0 = 0b00;
         const RPL_1 = 0b01;
         const RPL_2 = 0b10;
         const RPL_3 = 0b11;
         const TI_GDT = 0 << 2;
         const TI_LDT = 1 << 2;
+    }
+}
+
+struct GdtAccessFlags {}
+
+impl GdtAccessFlags {
+    const NULL: u8 = 0;
+    const PRESENT: u8 = 1 << 7;
+    const RING_0: u8 = 0 << 5;
+    const RING_3: u8 = 3 << 5;
+    const SYSTEM: u8 = 1 << 4;
+    const EXECUTABLE: u8 = 1 << 3;
+    const PRIVILEGE: u8 = 1 << 1;
+    const TSS_AVAIL: u8 = 9;
+}
+
+bitflags::bitflags! {
+    struct GdtEntryFlags: u8 {
+        const NULL = 0;
+        const PROTECTED_MODE = 1 << 6;
+        const LONG_MODE = 1 << 5;
     }
 }
 
@@ -65,25 +131,16 @@ pub struct GdtEntry {
 }
 
 impl GdtEntry {
-    const NULL: Self = Self::new(0x00, 0x00, 0x00, 0x00, 0x00, 0x00);
+    const NULL: Self = Self::new(GdtAccessFlags::NULL, GdtEntryFlags::NULL);
 
-    /// Create a new GDT entry.
-    #[inline]
-    pub const fn new(
-        limit_low: u16,
-        base_low: u16,
-        base_middle: u8,
-        access_byte: u8,
-        limit_hi_flags: u8,
-        base_hi: u8,
-    ) -> Self {
+    const fn new(access_flags: u8, entry_flags: GdtEntryFlags) -> Self {
         Self {
-            limit_low,
-            base_low,
-            base_middle,
-            access_byte,
-            limit_hi_flags,
-            base_hi,
+            limit_low: 0x00,
+            base_low: 0x00,
+            base_middle: 0x00,
+            access_byte: access_flags,
+            limit_hi_flags: entry_flags.bits() & 0xF0,
+            base_hi: 0x00,
         }
     }
 }
@@ -92,93 +149,36 @@ impl GdtEntry {
 ///
 /// **Notes**: <https://wiki.osdev.org/Task_State_Segment>
 #[derive(Debug, Clone, Copy)]
-#[repr(C, packed)]
-pub struct TssEntry {
-    /// The previous TSS - with hardware task switching these form a kind of backward linked list.
-    previous_tss: u64,
-    /// The stack pointer to load when changing to kernel mode.
-    esp0: u64,
-    /// The stack segment to load when changing to kernel mode.
-    ss0: u64,
-    esp1: u64,
-    ss1: u64,
-    esp2: u64,
-    ss2: u64,
-    cr3: u64,
-    eip: u64,
-    eflags: u64,
-    eax: u64,
-    ecx: u64,
-    edx: u64,
-    ebx: u64,
-    esp: u64,
-    ebp: u64,
-    esi: u64,
-    edi: u64,
-    es: u64,
-    cs: u64,
-    ss: u64,
-    ds: u64,
-    fs: u64,
-    gs: u64,
-    ldt: u64,
-    trap: u16,
+#[repr(C, align(16))]
+struct TssEntry {
+    reserved: u32,
+    rsp: [u64; 3],
+    reserved2: u64,
+    ist: [u64; 7],
+    reserved3: u64,
+    reserved4: u16,
     iomap_base: u16,
 }
 
 impl TssEntry {
     #[inline]
-    pub const fn new() -> Self {
+    const fn new() -> Self {
         Self {
-            previous_tss: 0,
-            esp0: 0,
-            ss0: 0,
-            esp1: 0,
-            ss1: 0,
-            esp2: 0,
-            ss2: 0,
-            cr3: 0,
-            eip: 0,
-            eflags: 0,
-            eax: 0,
-            ecx: 0,
-            edx: 0,
-            ebx: 0,
-            esp: 0,
-            ebp: 0,
-            esi: 0,
-            edi: 0,
-            es: 0,
-            cs: 0,
-            ss: 0,
-            ds: 0,
-            fs: 0,
-            gs: 0,
-            ldt: 0,
-            trap: 0,
-            iomap_base: mem::size_of::<Self>() as u16,
+            reserved: 0,
+            rsp: [0; 3],
+            reserved2: 0,
+            ist: [0; 7],
+            reserved3: 0,
+            reserved4: 0,
+            iomap_base: 0xFFFF,
         }
-    }
-
-    pub fn as_gdt_entry(self) -> GdtEntry {
-        let base = (&self as *const Self) as usize;
-        let limit = base + mem::size_of::<Self>();
-
-        GdtEntry::new(0, 0, base as u8, limit as u8, 0xE9, 0x00)
     }
 }
 
 /// Initialize the GDT.
 pub fn init() {
     unsafe {
-        let tss = TssEntry::new().as_gdt_entry();
-
-        GDT[0] = GdtEntry::new(0, 0, 0, 0x00, 0x00, 0);
-        GDT[1] = GdtEntry::new(0, 0, 0, 0x9A, 0xA0, 0);
-        GDT[2] = GdtEntry::new(0, 0, 0, 0x92, 0xA0, 0);
-        GDT[3] = GdtEntry::new(0, 0, 0, 0xFA, 0xA0, 0);
-        GDT[4] = GdtEntry::new(0, 0, 0, 0xF2, 0xA0, 0);
-        GDT[5] = tss;
+        TssEntry::new();
 
         let gdt_descriptor = GdtDescriptor::new(
             (mem::size_of::<[GdtEntry; GDT_ENTRIES]>() - 1) as u16,
@@ -194,8 +194,7 @@ pub fn init() {
         load_fs(SegmentSelector::new(2, SegmentSelector::RPL_0));
         load_gs(SegmentSelector::new(3, SegmentSelector::RPL_0));
         load_ss(SegmentSelector::new(2, SegmentSelector::RPL_0));
-
-        load_tss(&tss as *const _);
+        load_tss(SegmentSelector::new(8, SegmentSelector::RPL_0));
     }
 }
 
@@ -238,11 +237,11 @@ unsafe fn load_ss(selector: SegmentSelector) {
 }
 
 #[inline(always)]
-unsafe fn load_gdt(gdt_descriptor: *const GdtDescriptor) {
-    asm!("lgdt [rdi]", in("rdi") gdt_descriptor)
+unsafe fn load_tss(selector: SegmentSelector) {
+    asm!("ltr [rdi]", in("rdi") selector.bits(), options(nomem, nostack))
 }
 
 #[inline(always)]
-unsafe fn load_tss(gdt_entry: *const GdtEntry) {
-    asm!("ltr [rdi]", in("rdi") gdt_entry)
+unsafe fn load_gdt(gdt_descriptor: *const GdtDescriptor) {
+    asm!("lgdt [rdi]", in("rdi") gdt_descriptor, options(nostack))
 }
