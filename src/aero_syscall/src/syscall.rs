2cadb491472f16bfde725dfc137d5fd703e5a9ca
unknown <andypythonappdeveloper@gmail.com>
2021-05-11 14:54:57 +1000

    Pointer width check in supports_syscall_sysret

--- src/aero_syscall/src/syscall.rs
+++ src/aero_syscall/src/syscall.rs
@@ -1,7 +1,7 @@
 use raw_cpuid::CpuId;
 use spin::Once;
 
-macro __impl_syscall_fn($(pub fn $sys_fn:ident($a:ident $(,$b:ident $(,$c:ident $(,$d:ident $(,$e:ident $(,$f:ident)?)?)?)?)?) -> usize;)+) {
+macro define_syscall_fns($(pub fn $sys_fn:ident($a:ident $(,$b:ident $(,$c:ident $(,$d:ident $(,$e:ident $(,$f:ident)?)?)?)?)?) -> usize;)+) {
     $(
         pub unsafe fn $sys_fn(mut $a: usize, $($b: usize, $($c: usize, $($d: usize, $($e: usize, $($f: usize)?)?)?)?)?) -> usize {
             if $crate::syscall::supports_syscall_sysret() {
@@ -29,7 +29,7 @@ macro __impl_syscall_fn($(pub fn $sys_fn:ident($a:ident $(,$b:ident $(,$c:ident 
     )+
 }
 
-__impl_syscall_fn!(
+define_syscall_fns!(
     pub fn syscall0(a) -> usize;
     pub fn syscall1(a, b) -> usize;
     pub fn syscall2(a, b, c) -> usize;
@@ -41,13 +41,23 @@ __impl_syscall_fn!(
 /// Returns true if the current CPU supports the `syscall` and
 /// the `sysret` instruction.
 pub fn supports_syscall_sysret() -> bool {
-    static CACHE: Once<bool> = Once::new(); // This will cache the result.
+    cfg_if::cfg_if!(
+        if #[cfg(target_pointer_width = "64")] {
+            static CACHE: Once<bool> = Once::new(); // Cache the result of `cpuid` feature info.
 
-    *CACHE.call_once(|| {
-        let function_info = CpuId::new()
-            .get_extended_function_info()
-            .expect("Failed to retrieve CPU function info");
+            return *CACHE.call_once(|| {
+                let function_info = CpuId::new()
+                    .get_extended_function_info()
+                    .expect("Failed to retrieve CPU function info");
 
-        function_info.has_syscall_sysret()
-    })
+                function_info.has_syscall_sysret()
+            });
+        } else {
+            /*
+             * We are sure if the target pointer width is not 64 then syscall instruction is
+             * not supported.
+             */
+            return false;
+        }
+    );
 }
