bfb38867ffc3b6c6eac3edd7dbd0fc96bba21650
unknown <andypythonappdeveloper@gmail.com>
2021-05-16 15:35:14 +1000

    Create the path struct

--- src/aero_kernel/src/fs/mod.rs
+++ src/aero_kernel/src/fs/mod.rs
@@ -21,6 +21,33 @@ pub(super) fn install_filesystem<F: 'static + FileSystem>(
     FILE_SYSTEMS.lock().insert(signature, filesystem);
 }
 
+pub enum AeroInvalidPath {
+    /// If path's length is greater then `4096` (ie. The max path size in characters).
+    HugePath,
+}
+
+/// Structure that represents a path in a file system. This structure is a transparent
+/// wrapper around ref [str].
+#[repr(transparent)]
+pub struct Path<'path>(&'path str);
+
+impl<'path> Path<'path> {
+    /// Aero uses `4096` as the maximum path size in characters.
+    pub const MAX_PATH_SIZE: usize = 4096;
+
+    pub fn new(path: &'path str) -> Result<Self, AeroInvalidPath> {
+        /*
+         * Make sure we do not accept any paths greater then the maximum path
+         * size.
+         */
+        if path.len() > Self::MAX_PATH_SIZE {
+            return Err(AeroInvalidPath::HugePath);
+        }
+
+        Ok(Self(path))
+    }
+}
+
 pub fn init() {
     devfs::init();
 }
