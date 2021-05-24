49d6b23351684490a5cd604f4999715398ae7b76
unknown <andypythonappdeveloper@gmail.com>
2021-05-24 17:22:39 +1000

    Docs about how the standard library works (hacky)

--- userland/std/src/entry.rs
+++ userland/std/src/entry.rs
@@ -1,6 +1,20 @@
+/*
+ * Copyright 2021 The Aero Project Developers. See the COPYRIGHT
+ * file at the top-level directory of this project.
+ *
+ * Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+ * http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+ * <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+ * option. This file may not be copied, modified, or distributed
+ * except according to those terms.
+ */
+
 #[no_mangle]
 unsafe extern "C" fn _start() -> ! {
     extern "C" {
+        /// The rustc compiler auto generates a main function for us and its definition
+        /// is fixed on all platforms. We still have to use "C" ABI (as defined in rustc)
+        /// because rust ABI is currently not stable *yet*.
         fn main(argc: isize, argv: *const *const u8) -> isize;
     }
 
