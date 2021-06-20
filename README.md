e3e05194a8c059904eb8023e025958a3a9016485
unknown <andypythonappdeveloper@gmail.com>
2021-06-20 18:22:37 +1000

    Re-license to GPLv3
    * Re-license the whole project to GPLv3
    * Add the copyright headers on some  missing files
    * Update the LISCENSE file
    
    Signed By: Anhad Singh

--- README.md
+++ README.md
@@ -92,11 +92,7 @@ Contributions are absolutely, positively welcome and encouraged! Check out [CONT
 
 # License
 
-Licensed under either of
-
-- Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
-- MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
-
-at your option.
-
-Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in the work by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any additional terms or conditions.
+Aero is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version. See the [LICENSE](LICENSE) file for license rights and limitations
--- src/aero_kernel/src/arch/aarch64/mod.rs
+++ src/aero_kernel/src/arch/aarch64/mod.rs
@@ -1,10 +1,18 @@
-/*
- * Copyright 2021 The Aero Project Developers. See the COPYRIGHT
- * file at the top-level directory of this project.
+/* 
+ * Copyright (C) 2021 The Aero Project Developers.
+ * 
+ * This file is part of The Aero Project.
  *
- * Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
- * http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
- * <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
- * option. This file may not be copied, modified, or distributed
- * except according to those terms.
+ * Aero is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Aero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Aero. If not, see <https://www.gnu.org/licenses/>.
  */
