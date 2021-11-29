8ff51e8c4edfeb632d1b75de800b4fbc0da35a28
czapek1337 <czapek1337@gmail.com>
2021-11-29 01:00:26 +0100

    kernel, syscall: remove the legacy int 80h syscall method

--- src/aero_syscall/src/syscall.rs
+++ src/aero_syscall/src/syscall.rs
@@ -1,28 +1,15 @@
 macro define_syscall_fns($(pub fn $sys_fn:ident($a:ident $(,$b:ident $(,$c:ident $(,$d:ident $(,$e:ident $(,$f:ident $(,$g:ident)?)?)?)?)?)?) -> usize;)+) {
     $(
         pub fn $sys_fn(mut $a: usize, $($b: usize, $($c: usize, $($d: usize, $($e: usize, $($f: usize, $($g: usize)?)?)?)?)?)?) -> usize {
-            if $crate::syscall::supports_syscall_sysret() {
-                unsafe {
+            unsafe {
                     asm!(
                         "syscall",
-                        inout("rax") $a,
-                        $(in("rdi") $b, $(in("rsi") $c, $(in("rdx") $d, $(in("r10") $e, $(in("r8") $f,)?)?)?)?)?
-                        out("rcx") _,
-                        out("r11") _,
-                        options(nostack),
-                    );
-             }
-            } else {
-                unsafe {
-                    asm!(
-                        "int 0x80",
                         inout("rax") $a,
                         $(in("rdi") $b, $(in("rsi") $c, $(in("rdx") $d, $(in("r10") $e, $(in("r8") $f, $(in("r9") $g,)?)?)?)?)?)?
                         out("rcx") _,
                         out("r11") _,
                         options(nostack),
                     );
-                }
             }
 
             $a
@@ -39,18 +26,3 @@ define_syscall_fns!(
     pub fn syscall5(a, b, c, d, e, f) -> usize;
     pub fn syscall6(a, b, c, d, e, f, g) -> usize;
 );
-
-/// Returns true if the current CPU supports the `syscall` and
-/// the `sysret` instruction.
-#[inline(always)]
-pub fn supports_syscall_sysret() -> bool {
-    #[cfg(target_pointer_width = "64")]
-    {
-        true
-    }
-
-    #[cfg(not(target_pointer_width = "64"))]
-    {
-        false
-    }
-}
