641b8b7b9feedb91d9fbdd445065e349316530e5
Andy-Python-Programmer <andypythonappdeveloper@gmail.com>
2022-01-20 17:11:16 +1100

    gdb_debug_userland: be able to debug a library aswell
    Signed-off-by: Andy-Python-Programmer <andypythonappdeveloper@gmail.com>

--- tools/gdb-debug-userland.py
+++ tools/gdb-debug-userland.py
@@ -5,18 +5,27 @@ import os
 import subprocess
 
 
-def gdb_main(program: str):
+def gdb_main(program: str, is_lib: bool):
     print("connecting to the GDB server")
     # connect to gdb-server
     gdb.execute("target remote localhost:1234")
 
     print("loading the symbol table")
-    # load the symbol table
-    gdb.execute("symbol-file sysroot/system-root/usr/bin/{}".format(program))
 
-    print("setting a breakpoint at the entry point")
-    # set a breakpoint at the entry point of the program
-    gdb.execute("b main")
+    if not is_lib:
+        # load the symbol table
+        gdb.execute(
+            "symbol-file sysroot/system-root/usr/bin/{}".format(program))
+    else:
+        # load the symbol table
+        gdb.execute(
+            "symbol-file sysroot/system-root/usr/lib/{}.so".format(program))
+
+    if not is_lib:
+        print("setting a breakpoint at the entry point")
+
+        # set a breakpoint at the entry point of the program
+        gdb.execute("b main")
 
 
 if __name__ == "__main__":
@@ -26,11 +35,14 @@ if __name__ == "__main__":
         # Since GDB does not let us pass arguments to the script and we have a big
         # brain, so we pass the arguments to the program as enviornment variables.
         program = os.getenv("AERO_DEBUG_USERLAND_PROGRAM")
-        gdb_main(program)
+        is_lib = os.environ.get("IS_LIB") == "yes"
+
+        gdb_main(program, is_lib)
 
     else:
         os.environ["AERO_DEBUG_USERLAND_PROGRAM"] = sys.argv[1]
         os.environ["IN_GDB"] = "yes"
+        os.environ["IS_LIB"] = "no"
 
         process = subprocess.Popen(
             "gdb -tui -q -x tools/gdb-debug-userland.py", shell=True)
