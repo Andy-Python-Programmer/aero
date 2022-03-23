5a9a8b6bd86b98258a52ba8f52fd5e2dcf70e70f
YusufKhan-improves-shit <98565009+YusufKhan-improves-shit@users.noreply.github.com>
2022-03-22 23:32:31 -0700

    aero_kernel: arch: aarch64: add amd64 wrapper

---
+++ src/aero_kernel/src/arch/aarch64/amd64_wrapper.asm
@@ -0,0 +1,29 @@
+; Copyright (C) 2022 Yusuf Khan.
+;
+; This file is part of The Aero Project.
+;
+; Aero is free software: you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+;
+; Aero is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with Aero. If not, see <https://www.gnu.org/licenses/>. 
+
+;This is meant to be a place to deposit wrappers for amd64 devices
+;on aarch64.
+
+#define	rax r0
+#define	rbx r1
+#define	rcx r2
+#define	rdx r3
+#define	rsi r4
+#define	rdi r5
+#define rbp r11
+#define rsp r13
+#define rip r15
