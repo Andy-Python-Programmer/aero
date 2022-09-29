d5cc3c698e1457f2665b356d91536b16d76a4201
Joshua Price <jwpjr567@gmail.com>
2022-09-29 18:03:20 +0000

    build: remove macro

--- src/aero_build/src/constants.rs
+++ src/aero_build/src/constants.rs
@@ -1,24 +1,13 @@
-use std::path::Path;
+pub const OVMF_URL: &'static str = "https://github.com/aero-os/ovmf-prebuilt";
+pub const LIMINE_URL: &'static str = "https://github.com/limine-bootloader/limine";
 
-#[macro_export]
-macro_rules! const_str {
-    ($name:ident, $value:expr) => {
-        pub const $name: &'static str = $value;
-    };
-}
-
-const_str!(OVMF_URL, "https://github.com/aero-os/ovmf-prebuilt");
-
-// pub const OVMF_URL = "https://github.com/aero-os/ovmf-prebuilt";
-const_str!(LIMINE_URL, "https://github.com/limine-bootloader/limine");
-
-const_str!(BUILD_DIR, "build");
-const_str!(BUNDLED_DIR, "bundled");
-const_str!(SYSROOT_DIR, "sysroot");
-const_str!(SYSROOT_CARGO_HOME, "sysroot/cargo-home");
-const_str!(EXTRA_FILES, "extra-files");
-const_str!(BASE_FILES_DIR, "base-files");
-const_str!(LIMINE_TEMPLATE, r#"
+pub const BUILD_DIR: &'static str = "build";
+pub const BUNDLED_DIR: &'static str = "bundled";
+pub const SYSROOT_DIR: &'static str = "sysroot";
+pub const SYSROOT_CARGO_HOME: &'static str = "sysroot/cargo-home";
+pub const EXTRA_FILES: &'static str = "extra-files";
+pub const BASE_FILES_DIR: &'static str = "base-files";
+pub const LIMINE_TEMPLATE: &'static str = r#"
 TIMEOUT=0
 VERBOSE=yes
 
@@ -33,4 +22,4 @@ MODULE_CMDLINE=background
 
 MODULE_PATH=boot:///initramfs.cpio
 MODULE_CMDLINE=initramfs
-"#);
+"#;
