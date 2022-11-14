92d03cfb0e6bb0f84500a9fec5d23161e9613a2e
amada95 <amada.mackey@utexas.edu>
2022-11-14 02:21:48 -0600

    Fix xbstrap bug in aero.py, add opendoas support

--- tools/mkimage.sh
+++ tools/mkimage.sh
@@ -11,25 +11,34 @@ IMAGE_PATH=build/disk.img
 # make the disk image
 rm -rf $IMAGE_PATH
 
+# set $(SUID_BINARY) based on installed SUID binary
+if ! command -v sudo &> /dev/null
+then
+	echo "mkimage.sh: sudo not found, attempting to use opendoas"
+	SUID_BINARY=$(which doas)
+else
+	SUID_BINARY=$(which sudo)
+fi
+
 dd if=/dev/zero bs=1G count=0 seek=512 of=$IMAGE_PATH
 parted -s $IMAGE_PATH mklabel gpt
 parted -s $IMAGE_PATH mkpart primary 2048s 100%
-sudo losetup -Pf --show $IMAGE_PATH > loopback_dev
-sudo mkfs.ext2 `cat loopback_dev`p1 -I128
+$(SUID_BINARY) losetup -Pf --show $IMAGE_PATH > loopback_dev
+$(SUID_BINARY) mkfs.ext2 `cat loopback_dev`p1 -I128
 rm -rf disk_image/
 mkdir disk_image
-sudo mount `cat loopback_dev`p1 disk_image
-sudo cp -r -v sysroot/system-root/. disk_image/
+$(SUID_BINARY) mount `cat loopback_dev`p1 disk_image
+$(SUID_BINARY) cp -r -v sysroot/system-root/. disk_image/
 pushd disk_image
-sudo mkdir -p dev
-sudo mkdir -p home
-sudo mkdir -p tmp
-sudo mkdir -p proc
-sudo mkdir -p var
-sudo mkdir -p mnt
+$(SUID_BINARY) mkdir -p dev
+$(SUID_BINARY) mkdir -p home
+$(SUID_BINARY) mkdir -p tmp
+$(SUID_BINARY) mkdir -p proc
+$(SUID_BINARY) mkdir -p var
+$(SUID_BINARY) mkdir -p mnt
 popd
 sync
-sudo umount disk_image/
-sudo losetup -d `cat loopback_dev`
+$(SUID_BINARY) umount disk_image/
+$(SUID_BINARY) losetup -d `cat loopback_dev`
 sync
 rm -rf loopback_dev
