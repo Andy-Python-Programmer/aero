49de4dfc29a155964c5578aa7539fb474145e800
Andy-Python-Programmer <andypythonappdeveloper@gmail.com>
2023-03-02 17:50:34 +1100

    init: ensure the std{in,out,err} are avaliable before the X session is started
    Signed-off-by: Andy-Python-Programmer <andypythonappdeveloper@gmail.com>

--- userland/apps/init/src/main.rs
+++ userland/apps/init/src/main.rs
@@ -18,26 +18,47 @@
  */
 
 use std::error::Error;
-use std::fs::OpenOptions;
+use std::fs::{File, OpenOptions};
+use std::os::fd::AsRawFd;
 use std::process::Command;
-
-use std::mem;
 
 const TTY_PATH: &str = "/dev/tty";
 
+fn remove_cloexec(file: &File) {
+    // By default rust automatically sets the close-on-exe flag to prevent
+    // leaking file descriptors.
+    //
+    // OpenOptions::custom_flags() only allows insertion of flags and are
+    // overwritten by the flags set by the standard library. So here, we
+    // need to manually update the flags after opening the file.
+    let fd = file.as_raw_fd();
+
+    unsafe {
+        assert!(libc::fcntl(fd, libc::F_SETFD, 0 /* flags */) == 0);
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     // Open the stdin, stdout and stderr file descriptors.
-    //
-    // mem::forget(): don't drop the object which in turn will close the
-    // file.
-    mem::forget(OpenOptions::new().read(true).open(TTY_PATH)?); // fd=0 for stdin
-    mem::forget(OpenOptions::new().write(true).open(TTY_PATH)?); // fd=1 for stdout
-    mem::forget(OpenOptions::new().write(true).open(TTY_PATH)?); // fd=2 for stderr
+    let stdin = OpenOptions::new().read(true).open(TTY_PATH)?; // fd=0
+    let stdout = OpenOptions::new().write(true).open(TTY_PATH)?; // fd=1
+    let stderr = OpenOptions::new().write(true).open(TTY_PATH)?; // fd=2
+
+    remove_cloexec(&stdin);
+    remove_cloexec(&stdout);
+    remove_cloexec(&stderr);
 
     Command::new("dhcpd").spawn()?;
+
+    // Close the std{in,out,err} file descriptors, since now we are going to
+    // start an X session.
+    drop(stdin);
+    drop(stdout);
+    drop(stderr);
+
     Command::new("startx")
         .env("RUST_BACKTRACE", "full")
         .spawn()?;
 
-    loop {}
+    Ok(())
 }
