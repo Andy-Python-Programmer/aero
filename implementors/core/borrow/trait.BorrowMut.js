77aeda7f99c87cfb861b9f34b012923dc58bd3da
Andy-Python-Programmer <Andy-Python-Programmer@users.noreply.github.com>
2023-10-04 08:12:08 +0000

    deploy: 811fea0d0632949b425bfaae6253c8b45a78de7b

--- implementors/core/borrow/trait.BorrowMut.js
+++ implementors/core/borrow/trait.BorrowMut.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
 "aero_kernel":[],
 "allocator_api2":[["impl&lt;T: ?Sized, A: <a class=\"trait\" href=\"allocator_api2/alloc/trait.Allocator.html\" title=\"trait allocator_api2::alloc::Allocator\">Allocator</a>&gt; BorrowMut&lt;T&gt; for <a class=\"struct\" href=\"allocator_api2/boxed/struct.Box.html\" title=\"struct allocator_api2::boxed::Box\">Box</a>&lt;T, A&gt;"]],
-"arrayvec":[["impl&lt;const CAP: usize&gt; BorrowMut&lt;str&gt; for <a class=\"struct\" href=\"arrayvec/struct.ArrayString.html\" title=\"struct arrayvec::ArrayString\">ArrayString</a>&lt;CAP&gt;"],["impl&lt;T, const CAP: usize&gt; BorrowMut&lt;[T]&gt; for <a class=\"struct\" href=\"arrayvec/struct.ArrayVec.html\" title=\"struct arrayvec::ArrayVec\">ArrayVec</a>&lt;T, CAP&gt;"]]
+"arrayvec":[["impl&lt;T, const CAP: usize&gt; BorrowMut&lt;[T]&gt; for <a class=\"struct\" href=\"arrayvec/struct.ArrayVec.html\" title=\"struct arrayvec::ArrayVec\">ArrayVec</a>&lt;T, CAP&gt;"],["impl&lt;const CAP: usize&gt; BorrowMut&lt;str&gt; for <a class=\"struct\" href=\"arrayvec/struct.ArrayString.html\" title=\"struct arrayvec::ArrayString\">ArrayString</a>&lt;CAP&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
