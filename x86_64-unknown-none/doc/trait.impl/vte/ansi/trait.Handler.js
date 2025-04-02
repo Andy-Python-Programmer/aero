4f214c08a6cdcfcf19f7b2eecc69a1f5f4981d6f
Andy-Python-Programmer <Andy-Python-Programmer@users.noreply.github.com>
2025-04-02 07:07:27 +0000

    deploy: adbe0ad6e0f168111a0cda81fe48ef089727bc8a

--- x86_64-unknown-none/doc/trait.impl/vte/ansi/trait.Handler.js
+++ x86_64-unknown-none/doc/trait.impl/vte/ansi/trait.Handler.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["aero_kernel",[["impl&lt;'a&gt; <a class=\"trait\" href=\"vte/ansi/trait.Handler.html\" title=\"trait vte::ansi::Handler\">Handler</a> for <a class=\"struct\" href=\"aero_kernel/rendy/struct.Inner.html\" title=\"struct aero_kernel::rendy::Inner\">Inner</a>&lt;'a&gt;"]]]]);
+    var implementors = Object.fromEntries([["aero_kernel",[["impl <a class=\"trait\" href=\"vte/ansi/trait.Handler.html\" title=\"trait vte::ansi::Handler\">Handler</a> for <a class=\"struct\" href=\"aero_kernel/rendy/struct.Inner.html\" title=\"struct aero_kernel::rendy::Inner\">Inner</a>&lt;'_&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[271]}
+//{"start":57,"fragment_lengths":[261]}
